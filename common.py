5bf85fdffd4997d55d73213389619259f09a0f36
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-08-22 11:11:53 +0700

    add url validation

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 """
 is_connection_ok()
@@ -76,3 +77,18 @@ def who_is(host, server=None):
 
 # print(who_is('neucentrix.co.id'))
 # print(next(who_is('neucentrix.co.id')))
+
+"""
+is_valid_url(host)
+"""
+def is_valid_url(host):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://|' # http:// or https:// or
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, host) is not None
+
+#print(is_valid_url('http://wuriyanto.com'))
